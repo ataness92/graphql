3b8574cafd8e6486dc1f229839f75d17c1de4612
Atanas Tsonev <ataness92@gmail.com>
2021-06-14 16:45:30 +0300

    Changes

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ import apollo from 'apollo-server-express';
 import dotenv from "dotenv";
 import userTypes from "./graphql/types/user.js";
 import userResolver from "./graphql/resolvers/user.js";
+import mongoose from "mongoose"
 
 const { ApolloServer} = apollo
 
 dotenv.config();
 
+const db = process.env.MONGODB_URL;
 
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    autoIndex: true,
+    useCreateIndex: true,
+}).then(() =>{
+    console.log('connected to monodb')
+}).catch((e)=> {
+    console.log(e);
+})
 
 async function startApolloServer() {
     const server = new ApolloServer({
